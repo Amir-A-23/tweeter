5754ebbb53e7d61062d973d43c91659e45e6a34d
Amir <amir.a.23@hotmail.com>
2021-11-18 17:14:48 +0000

    Add GET request within the callback to the POST to display the latest tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const renderTweets = function(tweets) {
+const renderTweets = tweets => {
   // loops through tweets
   // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+  // takes return value and prepend it to the tweets container
   for(let tweetData of tweets) {
-    //console.log(tweetData);
     let $tweetElement = createTweetElement(tweetData);
     $(`.tweet-container`).prepend($tweetElement);
   }
-}
-createTweetElement = (tweetData) => {
+};
+const renderLatestTweet = tweets => {
+    const latestTweet = tweets[tweets.length - 1];
+    let $tweetElement = createTweetElement(latestTweet);
+    $(`.tweet-container`).prepend($tweetElement);
+};
+createTweetElement = tweetData => {
 let article = `<article class = "tweet">
                 <header>
                   <div class="avatar-name-container">
@@ -34,7 +38,7 @@ let article = `<article class = "tweet">
                 </div>
                 <footer>
                   <div class ="time-posted">
-                      <p>${tweetData.created_at}</p>
+                      <p>${timeago.format(tweetData.created_at)}</p>
                   </div>
                   <div class ="footer-icons">
                     <i class="fas fa-flag"></i>
@@ -45,29 +49,70 @@ let article = `<article class = "tweet">
               </article>`
   
   return article;
-}
+};
 
 
-$(document).ready(function() {
-  const loadTweets = () => {
-    $.get('/tweets/', function(data) {
-      renderTweets(data);
-      console.log(data);
-    });
-  }
+$(document).ready(() => {
+
   loadTweets();
-    $('#form-submit').submit(function(event) {
-    //alert( "Handler for .submit() called." );
-    const text = $('form').serialize();
-    //const textLength = text.length - 5; //minus 5 characters for "text="
-    //console.log(text);
-    $.post('/tweets', text, function () {
-      console.log(text);
+    
+    $('#form-submit').submit((event) => {
+    const formData = $('form').serialize();
+    const text = getText(formData);
+    if(!text || !text.trim()) {
+      alert('Error: Tweet is empty');
+    } else if(text.length > 140){
+      alert('Error: Exceeded Character Limit');
+    } else {
+    $.post('/tweets', formData, function () {
+
       //once you post a tweet, get that post back and display it
+      loadLatestTweet();
+
+      //reset textarea after submitting
+    $('textarea').val("");
     });
-    //reset textarea after submitting
+    }
+    
     //reset counter after submitting
+    $('.counter').text(140);
+    $(".counter").css('color', '#545149');
+
     event.preventDefault();
 
   });
 });
+// errorhandle = text => {
+//   if(!text || !text.trim()) {
+//      alert('Error: Tweet is empty');
+//      return true;
+//   } 
+//   if(text.length > 140){
+//      alert('Error: Exceeded Character Limit');
+//      return true;
+//   }
+//   return ;
+// }
+
+
+loadTweets = () => {
+  $.get('/tweets/', function(data) {
+    renderTweets(data);
+  });
+};
+
+loadLatestTweet = () => {
+  $.get('/tweets/', function(data) {
+    renderLatestTweet(data);
+  });
+}
+getText = formData => {
+  let text = '';
+  for(let chars in formData) {
+    if(chars > 4) {
+      text += formData[chars];
+    }
+  }
+  return text;
+};
+
